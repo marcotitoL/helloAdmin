fee217b93e048639cc177647eb951cf1772806f8
marcotitoL <imgtocss3@gmail.com>
2022-05-13 10:21:46 -0600

    added some quick stats for the dashboard

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,31 @@
 [Authorize]
 public class IndexModel : PageModel{
-    public void OnGet(){
+    HttpClient _httpClient = null!;
 
+    HttpResponseMessage response = null!;
+
+    public int noOfUsers = 0;
+    public int noOfProducts = 0;
+
+    public int noOfCategories = 0;
+    
+    public IndexModel(IApiClient apiClient){
+        _httpClient = apiClient.getApiClient();
+    }
+    public async Task<IActionResult> OnGet(){
+        response = await _httpClient.GetAsync("users/count");
+
+        noOfUsers = Convert.ToInt32( response.Content.ReadAsStringAsync().Result );
+
+        response = await _httpClient.GetAsync("products/count");
+
+        noOfProducts = Convert.ToInt32( response.Content.ReadAsStringAsync().Result );
+
+        response = await _httpClient.GetAsync("category/count");
+
+        noOfCategories = Convert.ToInt32( response.Content.ReadAsStringAsync().Result );
+
+
+        return Page();
     }
 }
